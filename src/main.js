bfc1e2a8842bccc1483ec488450d6b4053574af2
nicoson <nicoson@163.com>
2017-08-11 16:52:58 +0800

    start version

---
+++ src/main.js
@@ -0,0 +1,33 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import VueMaterial from 'vue-material'
+import '../node_modules/vue-material/dist/vue-material.css'
+
+Vue.config.productionTip = false
+
+Vue.use(VueMaterial)
+Vue.material.registerTheme({
+  default: {
+    primary: 'blue',
+    accent: 'red'
+  },
+  green: {
+    primary: 'green',
+    accent: 'pink'
+  },
+  orange: {
+    primary: 'orange',
+    accent: 'green'
+  }
+})
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App></App>',
+  components: { App }
+})
